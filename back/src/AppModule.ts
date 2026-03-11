d4db5277ea63ba90a6efdac08d8ba260962a5535
JessFreak <maliy.olexander@lll.kpi.ua>
2026-03-11 09:58:14 +0200

    dack/added logger

--- back/src/AppModule.ts
+++ back/src/AppModule.ts
@@ -7,12 +7,38 @@ import { UserModule } from './modules/UserModule';
 import { ServiceModule } from './modules/ServiceModule';
 import { OrderModule } from './modules/OrderModule';
 import { HealthModule } from './modules/HealthModule';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       load: [config],
+    }),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        serializers: {
+          req: (req) => ({ method: req.method, url: req.url }),
+          res: (res) => ({ statusCode: res.statusCode }),
+        },
+        formatters: {
+          level: (label) => ({ level: label.toUpperCase() }),
+        },
+        messageKey: 'message',
+        timestamp: () => `,"timestamp":"${new Date().toISOString()}"`,
+        base: undefined,
+        transport: process.env.NODE_ENV !== 'production'
+          ? {
+            target: 'pino-pretty',
+            options: {
+              colorize: true,
+              singleLine: true,
+              ignore: 'pid,hostname,req,res',
+              messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms'
+            }
+          }
+          : undefined,
+      },
     }),
     PrismaModule,
     AuthModule,
